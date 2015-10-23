2131b6e332b6a112bdb1eb0250d1631c8e683bce
Federica <fedfack@gmail.com>
2015-10-23 15:14:16 +0200

    changes for pt ratio

--- fullAnalysisRoma/macro/TandP/templateMaker.C
+++ fullAnalysisRoma/macro/TandP/templateMaker.C
@@ -7,8 +7,10 @@
 
 using namespace std;
 
-#define ptBinsEB 9
-#define ptBinsEE 7
+#define ptBinsEB 5
+#define ptBinsEE 5
+
+
 #define etaBinsEB 1
 #define etaBinsEE 1
 
@@ -16,12 +18,18 @@ using namespace std;
 void templateMaker::Loop()
 {
   if (fChain == 0) return;
-
-  float ptInfEB[ptBinsEB] = {20.,30.,40.,50.,60.,80.,110.,150.,250.};
-  float ptSupEB[ptBinsEB] = {30.,40.,50.,60.,80.,110.,150.,250.,500.};
-  float ptInfEE[ptBinsEE] = {20.,30.,40.,50.,60.,80.,110.};
-  float ptSupEE[ptBinsEE] = {30.,40.,50.,60.,80.,110.,500.};
-
+  
+  float ptInfEB[ptBinsEB] = {0.,100.,200.,300.,400.};
+  float ptSupEB[ptBinsEB] = {100.,200.,300.,400.,500.};
+  float ptInfEE[ptBinsEE] = {0.,100.,200.,300.,400.};
+  float ptSupEE[ptBinsEE] = {100.,200.,300.,400.,500.};
+  
+  /*
+  float ptInfEB[1] = {0.};
+  float ptSupEB[1] = {500.};
+  float ptInfEE[1] = {0.};
+  float ptSupEE[1] = {500.};
+  */
   float etaInfEB[1] = {0.0};
   float etaSupEB[1] = {1.5};
   float etaInfEE[1] = {1.5};
@@ -29,27 +37,27 @@ void templateMaker::Loop()
 
 
   // histos for passing and failing probes     
-  TH1F *hMassEBpass[ptBinsEB][etaBinsEB], *hMassEEpass[ptBinsEE][etaBinsEE];
-  TH1F *hMassEBfail[ptBinsEB][etaBinsEB], *hMassEEfail[ptBinsEE][etaBinsEE];
-
+  TH1F *hPt_RatioEBpass[ptBinsEB][etaBinsEB], *hPt_RatioEEpass[ptBinsEE][etaBinsEE];
+  TH1F *hPt_RatioEBfail[ptBinsEB][etaBinsEB], *hPt_RatioEEfail[ptBinsEE][etaBinsEE];
+  
   for (int ii=0; ii<ptBinsEB; ii++) {
     for (int jj=0; jj<etaBinsEB; jj++) {
-      hMassEBpass[ii][jj] = new TH1F("hMassEBpass[ii][jj]","hMassEBpass[ii][jj]",80,70,110);
-      hMassEBpass[ii][jj]->Sumw2();
-      hMassEBfail[ii][jj] = new TH1F("hMassEBfail[ii][jj]","hMassEBfail[ii][jj]",80,70,110);
-      hMassEBfail[ii][jj]->Sumw2();
+      hPt_RatioEBpass[ii][jj] = new TH1F("hPt_RatioEBpass[ii][jj]","hPt_RatioEBpass[ii][jj]",80,0.0,10);
+      hPt_RatioEBpass[ii][jj]->Sumw2();
+      hPt_RatioEBfail[ii][jj] = new TH1F("hPt_RatioEBfail[ii][jj]","hPt_RatioEBfail[ii][jj]",80,0.0,10);
+      hPt_RatioEBfail[ii][jj]->Sumw2();
     }
   }
   for (int ii=0; ii<ptBinsEE; ii++) {
-    for (int jj=0; jj<etaBinsEE; jj++) {
-      hMassEEpass[ii][jj] = new TH1F("hMassEEpass[ii][jj]","hMassEEpass[ii][jj]",80,70,110);
-      hMassEEpass[ii][jj]->Sumw2();
-      hMassEEfail[ii][jj] = new TH1F("hMassEEfail[ii][jj]","hMassEEfail[ii][jj]",80,70,110);
-      hMassEEfail[ii][jj]->Sumw2();
-    }
+      for (int jj=0; jj<etaBinsEE; jj++) {
+	hPt_RatioEEpass[ii][jj] = new TH1F("hPt_RatioEEpass[ii][jj]","hPt_RatioEEpass[ii][jj]",80,0.0,10);
+	hPt_RatioEEpass[ii][jj]->Sumw2();
+	hPt_RatioEEfail[ii][jj] = new TH1F("hPt_RatioEEfail[ii][jj]","hPt_RatioEEfail[ii][jj]",80,0.0,10);
+	hPt_RatioEEfail[ii][jj]->Sumw2();
+      }
   }
-
-
+  
+  
   // Loop over entries    
   Long64_t nentries = fChain->GetEntriesFast();
   Long64_t nbytes = 0, nb = 0;
@@ -60,7 +68,7 @@ void templateMaker::Loop()
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
     if(jentry%10000==0) cout << jentry << endl;
-    
+        
     // passed or not 
     bool passed = false;
     if (probe_fullsel) passed = true;
@@ -69,8 +77,8 @@ void templateMaker::Loop()
       for (int jj=0; jj<etaBinsEB; jj++) {
 	if (probe_absEta>=etaInfEB[jj] && probe_absEta<=etaSupEB[jj]){ 
 	  if (probe_pt>=ptInfEB[ii] && probe_pt<=ptSupEB[ii]) { 
-	    if (passed) hMassEBpass[ii][jj]->Fill(mass);
-	    else hMassEBfail[ii][jj]->Fill(mass); 
+	    if (passed) hPt_RatioEBpass[ii][jj]->Fill(pt_ratio);
+	    else hPt_RatioEBfail[ii][jj]->Fill(pt_ratio); 
 	  }
 	}
       }
@@ -80,13 +88,13 @@ void templateMaker::Loop()
       for (int jj=0; jj<etaBinsEE; jj++) {
 	if (probe_absEta>=etaInfEE[jj] && probe_absEta<=etaSupEE[jj]){ 
 	  if (probe_pt>=ptInfEE[ii] && probe_pt<=ptSupEE[ii]) { 
-	    if (passed) hMassEEpass[ii][jj]->Fill(mass);
-	    else hMassEEfail[ii][jj]->Fill(mass); 
+	    if (passed) hPt_RatioEEpass[ii][jj]->Fill(pt_ratio);
+	    else hPt_RatioEEfail[ii][jj]->Fill(pt_ratio); 
 	  }
 	}
       }
     }
-
+    
   }  // Loop over entries
     
 
@@ -97,20 +105,20 @@ void templateMaker::Loop()
   for (int ii=0; ii<ptBinsEB; ii++) {
     for (int jj=0; jj<etaBinsEB; jj++) {
       TString thisName;
-      thisName = TString::Format("hMass_%f",ptInfEB[ii])+TString::Format("To%f",ptSupEB[ii])+TString::Format("_%f",etaInfEB[jj])+TString::Format("To%f",etaSupEB[jj])+"_Pass";
-      hMassEBpass[ii][jj] ->Write(thisName);
-      thisName = TString::Format("hMass_%f",ptInfEB[ii])+TString::Format("To%f",ptSupEB[ii])+TString::Format("_%f",etaInfEB[jj])+TString::Format("To%f",etaSupEB[jj])+"_Fail";
-      hMassEBfail[ii][jj] ->Write(thisName);
+      thisName = TString::Format("hPt_Ratio_%f",ptInfEB[ii])+TString::Format("To%f",ptSupEB[ii])+TString::Format("_%f",etaInfEB[jj])+TString::Format("To%f",etaSupEB[jj])+"_Pass";
+      hPt_RatioEBpass[ii][jj] ->Write(thisName);
+      thisName = TString::Format("hPt_Ratio_%f",ptInfEB[ii])+TString::Format("To%f",ptSupEB[ii])+TString::Format("_%f",etaInfEB[jj])+TString::Format("To%f",etaSupEB[jj])+"_Fail";
+      hPt_RatioEBfail[ii][jj] ->Write(thisName);
     }
   }
   
   for (int ii=0; ii<ptBinsEE; ii++) {
     for (int jj=0; jj<etaBinsEE; jj++) {
       TString thisName;
-      thisName = TString::Format("hMass_%f",ptInfEE[ii])+TString::Format("To%f",ptSupEE[ii])+TString::Format("_%f",etaInfEE[jj])+TString::Format("To%f",etaSupEE[jj])+"_Pass";
-      hMassEEpass[ii][jj] ->Write(thisName);
-      thisName = TString::Format("hMass_%f",ptInfEE[ii])+TString::Format("To%f",ptSupEE[ii])+TString::Format("_%f",etaInfEE[jj])+TString::Format("To%f",etaSupEE[jj])+"_Fail";
-      hMassEEfail[ii][jj] ->Write(thisName);
+      thisName = TString::Format("hPt_Ratio_%f",ptInfEE[ii])+TString::Format("To%f",ptSupEE[ii])+TString::Format("_%f",etaInfEE[jj])+TString::Format("To%f",etaSupEE[jj])+"_Pass";
+      hPt_RatioEEpass[ii][jj] ->Write(thisName);
+      thisName = TString::Format("hPt_Ratio_%f",ptInfEE[ii])+TString::Format("To%f",ptSupEE[ii])+TString::Format("_%f",etaInfEE[jj])+TString::Format("To%f",etaSupEE[jj])+"_Fail";
+      hPt_RatioEEfail[ii][jj] ->Write(thisName);
     }
   }
 }
